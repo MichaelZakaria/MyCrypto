15684559e192d517a98b9fdae5f867ae897687b4
Marco Zakaria <marcozakaria@yahoo.com>
2024-10-29 16:56:00 +0300

    Initial Commit

---
+++ MyCrypto/MyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCryptoApp.swift
+//  MyCrypto
+//
+//  Created by Marco on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct MyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
